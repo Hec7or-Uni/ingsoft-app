7e430188ecb9377c8dfc235f54b8b2df1823d0f2
hector toral <hectortoraltrabajos@gmail.com>
2022-12-11 13:27:16 +0100

    cambio de actividad

--- app/src/main/java/eina/unizar/ingsoftapp/ReservationActivity.java
+++ app/src/main/java/eina/unizar/ingsoftapp/ReservationActivity.java
@@ -1,6 +1,9 @@
 package eina.unizar.ingsoftapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,21 @@ public class ReservationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.room_activity_main);
+        setContentView(R.layout.reservation_activity_main);
+
+        // References
+        Button res = findViewById(R.id.reservations);
+        Button hab = findViewById(R.id.rooms);
+
+        // Mods
+
+        // Listeners
+        Intent intent = new Intent(ReservationActivity.this, RoomActivity.class);
+        hab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/eina/unizar/ingsoftapp/RoomActivity.java
+++ app/src/main/java/eina/unizar/ingsoftapp/RoomActivity.java
@@ -2,7 +2,10 @@ package eina.unizar.ingsoftapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RoomActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class RoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_activity_main);
+
+        // References
+        Button res = findViewById(R.id.reservations);
+        Button hab = findViewById(R.id.rooms);
+
+        // Mods
+
+        // Listeners
+        Intent intent = new Intent(RoomActivity.this, ReservationActivity.class);
+        res.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intent);
+            }
+        });
+
     }
 }
